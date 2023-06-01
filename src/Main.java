c616c9a095c66d1d7d2d4ee4974b5dcbf1393c2c
OEM <gurleva_ludmila@mail.ru>
2023-06-02 00:26:13 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int sachet = 100;//сумма начального счета
-        //int summa = 1100;//сумма пополнения
-        int summa = 300;//или выбрать эту сумму пополнения
-        int boncuff = 100;
+        int startingScore = 100;//сумма начального счета
+        int summa = 1100;//сумма пополнения
+        //int summa = 300;//или выбрать эту сумму пополнения
+        int addition = 100;
         int bonus = 1000;
 
         if (summa > bonus) {
-            int totsumma = summa / boncuff + summa + sachet;
-            System.out.println("Сумма бонусов = " +  (summa / boncuff));
-            System.out.println("Общая сумма = " + totsumma + "рублей");
+            int finalScore= summa / addition + summa + startingScore;
+            System.out.println("Сумма бонусов = " +  (summa / addition));
+            System.out.println("Итоговый счет = " + finalScore + " рублей");
         } else {
-            int totsumma = summa + sachet;
+            int finalScore = summa + startingScore;
             System.out.println("Сумма бонусов = " + 0);
-            System.out.println("Общая сумма = " + totsumma + "рублей");
+            System.out.println("Итоговый счет = " + finalScore + "рублей");
 
         }
     }
